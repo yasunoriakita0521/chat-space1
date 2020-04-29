62cb45ad5abe4dc66140828e933a84058047514d
yasunoriakita0521 <sonunder-0521aoi@softbank.ne.jp>
2020-04-29 09:24:18 +0900

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.13.0'
 
 # Capistranoのログの表示に利用する
-set :application, 'chat-space1'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:yasunoriakita0521/chat-space1.git'
